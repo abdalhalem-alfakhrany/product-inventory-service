fb35589a81d5dd4baca74691540782607223a9f8
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-24 22:03:43 +0300

    using pest for tests

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(ProductController::class)->prefix('products')->group(function () {
+    Route::get('/', 'index');
+    Route::post('/', 'store');
+    Route::get('/{id}', 'show');
+    Route::put('/{id}', 'update');
+    Route::delete('/{id}', 'destroy');
+
+    Route::post('/{id}/stock', 'adjust_stock');
+    Route::get('low-stock', 'low_stock');
 });
