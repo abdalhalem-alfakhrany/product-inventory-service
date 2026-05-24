c0bef9244e6b83b633df6d4c73e7ebebe137e918
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-24 20:56:05 +0300

    product factory created

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enum\ProductStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Ramsey\Uuid\Nonstandard\Uuid;
+use Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->randomElement(['Laptop', 'Headphones', 'Camera', 'Smartwatch']);
+        return [
+            'id' => Uuid::uuid7(),
+            'name' => $name,
+            'sku' => fake()->unique()->numerify('PRD-#####'),
+            'description' => fake()->paragraph(),
+            'price' => fake()->numberBetween(100, 2000),
+            'stock_quantity' => fake()->numberBetween(100, 500),
+            'status' => ProductStatus::Active,
+        ];
+    }
+}
