4a2b5ec34c143abb00033c68217f5d0bc9b02dff
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-24 23:09:21 +0300

    product api test updated

--- tests/Feature/ProductApiTest.php
+++ tests/Feature/ProductApiTest.php
@@ -2,10 +2,11 @@
 
 use App\Enum\ProductStatus;
 use App\Models\Product;
-use function Pest\Laravel\assertDatabaseCount;
 
 it('list all products with pagination', function () {
-    Product::factory()->status(ProductStatus::Active)->count(10)->create();
+    Product::factory()->status(ProductStatus::Active)->count(20)->create();
     $response = $this->getJson('/api/products/');
-    $response->assertStatus(200)->assertJson(['success' => true]);
+    $response->assertStatus(200);
+    $response->assertJsonPath('success', true);
+    $response->assertJsonCount(20, 'data');
 });
