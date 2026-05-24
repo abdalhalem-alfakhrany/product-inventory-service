1393188607d2b609be4102789291821028bcc3fb
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-24 21:20:04 +0300

    product repository created

---
+++ app/Repositories/ProductRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
+class ProductRepository implements ProductRepositoryInterface
+{
+    public function all(): Collection
+    {
+        return Product::all();
+    }
+
+    public function create(array $data): ?Product
+    {
+        return Product::create($data);
+    }
+
+    public function update(array $data, int $id): int
+    {
+        $product = Product::findOrFail($id);
+        return $product->update($data);
+    }
+
+    public function delete(int $id): bool
+    {
+        $product = Product::findOrFail($id);
+        return $product->delete();
+    }
+
+    public function find(int $id): ?Product
+    {
+        return Product::findOrFail($id);
+    }
+}
---
+++ app/Repositories/ProductRepositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Collection;
+
+interface ProductRepositoryInterface
+{
+    public function all(): Collection;
+
+    public function create(array $data): ?Product;
+
+    public function update(array $data, int $id): int;
+
+    public function delete(int $id): bool;
+
+    public function find(int $id): ?Product;
+}
