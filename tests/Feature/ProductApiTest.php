c0673564ddc20883edd8b34bc2df9cc4e7062ee3
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-24 22:27:35 +0300

    ProductApiTest file create

---
+++ tests/Feature/ProductApiTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Enum\ProductStatus;
+use App\Models\Product;
+use function Pest\Laravel\assertDatabaseCount;
+
+it('list all products with pagination', function () {
+    Product::factory()->status(ProductStatus::Active)->count(10)->create();
+    $response = $this->getJson('/api/products/');
+
+    $response->assertStatus(200)
+        ->assertJsonStructure([
+            'data',
+            'meta' => ['pagination']
+        ]);
+});
