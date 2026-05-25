45241368d4a2ca555c34f80ae619c4f9842153f1
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-25 07:15:31 +0300

    product api test updated [can get one product,can create new product tests added]

--- tests/Feature/ProductApiTest.php
+++ tests/Feature/ProductApiTest.php
@@ -3,10 +3,39 @@
 use App\Enum\ProductStatus;
 use App\Models\Product;
 
-it('list all products with pagination', function () {
-    Product::factory()->status(ProductStatus::Active)->count(20)->create();
+it('can list all products', function () {
+    Product::factory()->status(ProductStatus::Active)->count(100)->create();
     $response = $this->getJson('/api/products/');
     $response->assertStatus(200);
-    $response->assertJsonPath('success', true);
-    $response->assertJsonCount(20, 'data');
+    $response->assertJsonStructure([
+        'success',
+        'data',
+        'meta' => [
+            'pagination' => [
+                'current_page'
+            ]
+        ]
+    ]);
+});
+
+it('can get one product', function () {
+    Product::factory()->status(ProductStatus::Active)->create();
+    $uuid = Product::first()->id;
+    $response = $this->getJson("/api/products/$uuid");
+    $response->assertStatus(200);
+    $response->assertJsonStructure([
+        'success',
+        'data',
+        'meta'
+    ]);
+});
+
+it('can create new product', function () {
+    $response = $this->postJson('/api/products', [
+        'name' => 'new product',
+        'description' => 'new product description',
+        'price' => 100,
+        'stock_quantity' => 10,
+    ]);
+    $response->assertStatus(200);
 });
