48836d3f5952d6b8cab0ff0cee8510d56ba0dde8
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2026-05-25 09:54:55 +0300

    product observer created

---
+++ app/Observers/ProductObserver.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Product;
+use Log;
+
+class ProductObserver
+{
+    /**
+     * Handle the Product "created" event.
+     */
+    public function created(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "updated" event.
+     */
+    public function updated(Product $product): void
+    {
+        if ($product->stock_quantity <= $product->low_stock_threshold) {
+            Log::alert("product $product->id stock is lower threshold");
+        }
+    }
+
+    /**
+     * Handle the Product "deleted" event.
+     */
+    public function deleted(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "restored" event.
+     */
+    public function restored(Product $product): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Product "force deleted" event.
+     */
+    public function forceDeleted(Product $product): void
+    {
+        //
+    }
+}
